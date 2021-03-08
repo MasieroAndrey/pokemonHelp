23b57270efd071dfcf648155fb1659b947ac1d28
Andrey Masiero <andrey.masiero@hotmail.com>
2021-03-08 10:26:03 -0300

    stackViewPokemon

---
+++ pokemonHelper/PokemonViewController.swift
@@ -0,0 +1,46 @@
+//
+//  PokemonViewController.swift
+//  pokemonHelper
+//
+//  Created by amasiero on 08/03/21.
+//
+
+import UIKit
+
+class PokemonViewController: UIViewController {
+
+	@IBOutlet weak var stackPokemonBeedril: UIStackView!
+	@IBOutlet weak var stackPokemonUnbreon: UIStackView!
+	@IBOutlet weak var stackPokemonBlestoide: UIStackView!
+	@IBOutlet weak var stackPokemonEevee: UIStackView!
+	@IBOutlet weak var stackPokemonGengar: UIStackView!
+	@IBOutlet weak var stackPokemonArcanine: UIStackView!
+	@IBOutlet weak var stackPokemonCharizard: UIStackView!
+	@IBOutlet weak var stackPokemonCorphish: UIStackView!
+	@IBOutlet weak var stackPokemonPikachu: UIStackView!
+	@IBOutlet weak var stackPokemonFebas: UIStackView!
+	@IBOutlet weak var stackPokemonPidgeot: UIStackView!
+	@IBOutlet weak var stackPokemonCharmander: UIStackView!
+	@IBOutlet weak var stackPokemonCrawdaunt: UIStackView!
+	@IBOutlet weak var stackPokemonDragonite: UIStackView!
+	@IBOutlet weak var stackPokemonPsyduck: UIStackView!
+	@IBOutlet weak var stackPokemonSylveon: UIStackView!
+
+	
+	override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- pokemonHelper/ViewController.swift
+++ pokemonHelper/ViewController.swift
@@ -9,39 +9,48 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
+	
+	var music: AVAudioPlayer?
+	
+	func playSound() {
+		guard let url = Bundle.main.url(forResource: "pokemonmusic", withExtension: "mp3") else { return }
+		do {
+			try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+			try AVAudioSession.sharedInstance().setActive(true)
+			
+			/* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+			music = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+			
+			/* iOS 10 and earlier require the following line:
+			player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+			
+			guard let music = music else { return }
+			
+			music.play()
+			music.numberOfLoops = 10 // pensar se é interessante deixar infinito ou apenas para abertura do APP
+			
+		} catch let error {
+			print(error.localizedDescription)
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
-        playSound()
+//        playSound()
 	}
 
 // ---
     //----------Função Música de fundo.
     //func fundomusic () {
-    var music: AVAudioPlayer?
 
-    func playSound() {
-        guard let url = Bundle.main.url(forResource: "pokemonmusic", withExtension: "mp3") else { return }
-        do {
-            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
-            try AVAudioSession.sharedInstance().setActive(true)
-
-            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
-            music = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
-
-            /* iOS 10 and earlier require the following line:
-            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
-
-            guard let music = music else { return }
-
-            music.play()
-            music.numberOfLoops = 10 // pensar se é interessante deixar infinito ou apenas para abertura do APP
-
-        } catch let error {
-            print(error.localizedDescription)
-        }
-    }
-    //-------------
 }
 
